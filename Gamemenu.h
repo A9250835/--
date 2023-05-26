6e65d519e551c88b1d66d0b04f527e94d9eccb46
Gary <79957959+A9250835@users.noreply.github.com>
2023-05-26 23:52:53 +0800

    Update Gamemenu.h

--- Gamemenu.h
+++ Gamemenu.h
@@ -1,3 +1,10 @@
+//
+//  GameMenu.hpp
+//  踩地雷
+//
+//  Created by Gary on 2023/5/25.
+//
+
 #ifndef GAME_MENU_H
 #define GAME_MENU_H
 
@@ -10,14 +17,19 @@ private:
 public:
     GameMenu();
     GameMenu(int numRows , int numCols , int numMines);
+    void gameInterface();
 
     void setBeginnerLevel();
     void setIntermediateLevel();
     void setAdvancedLevel();
+    void setUserDefined(int numRows , int numCols , int numMines);
 
     int getNumRows();
     int getNumCols();
     int getNumMines();
+    
+    void EnterRowColMines();
+    bool reasonableMines(int rows , int cols , int mines);
 };
 
 #endif
